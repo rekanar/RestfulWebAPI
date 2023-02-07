9bdb3091326de5af5f1492183c8654b97b79582e
Santhosh <santhoshrekanar@gmail.com>
2023-02-08 03:44:52 +0530

    my second commit

--- SuperVilla/SuperVilla/Controllers/WeatherForecastController.cs
+++ SuperVilla/SuperVilla/Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace SuperVilla.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorchxcxcvxvcing"
     };
 
         private readonly ILogger<WeatherForecastController> _logger;
